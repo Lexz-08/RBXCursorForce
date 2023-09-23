757deafbab5b3616796bb2df264356934f6c821c
nina <misterjeremiahjordan@live.com>
2023-09-22 23:26:37 -0700

    Add project files.

---
+++ Main.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace RBXCursorForce
+{
+	partial class Main
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+			this.label_RobloxStatusLabel = new NovaUI.Controls.NovaLabel();
+			this.label_RobloxStatus = new NovaUI.Controls.NovaLabel();
+			this.label_CursorForceStatusLabel = new NovaUI.Controls.NovaLabel();
+			this.label_CursorForceStatus = new NovaUI.Controls.NovaLabel();
+			this.novaPictureBox1 = new NovaUI.Controls.NovaPictureBox();
+			this.novaLabel1 = new NovaUI.Controls.NovaLabel();
+			this.novaLabel2 = new NovaUI.Controls.NovaLabel();
+			((System.ComponentModel.ISupportInitialize)(this.novaPictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// label_RobloxStatusLabel
+			// 
+			this.label_RobloxStatusLabel.AutoSize = true;
+			this.label_RobloxStatusLabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+			this.label_RobloxStatusLabel.Font = new System.Drawing.Font("Segoe UI", 12F);
+			this.label_RobloxStatusLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(235)))), ((int)(((byte)(235)))));
+			this.label_RobloxStatusLabel.Location = new System.Drawing.Point(12, 44);
+			this.label_RobloxStatusLabel.Name = "label_RobloxStatusLabel";
+			this.label_RobloxStatusLabel.Size = new System.Drawing.Size(61, 21);
+			this.label_RobloxStatusLabel.TabIndex = 0;
+			this.label_RobloxStatusLabel.Text = "Roblox:";
+			// 
+			// label_RobloxStatus
+			// 
+			this.label_RobloxStatus.AutoSize = true;
+			this.label_RobloxStatus.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+			this.label_RobloxStatus.Font = new System.Drawing.Font("Segoe UI", 12F);
+			this.label_RobloxStatus.ForeColor = System.Drawing.Color.Red;
+			this.label_RobloxStatus.Location = new System.Drawing.Point(120, 44);
+			this.label_RobloxStatus.Name = "label_RobloxStatus";
+			this.label_RobloxStatus.Size = new System.Drawing.Size(117, 21);
+			this.label_RobloxStatus.TabIndex = 1;
+			this.label_RobloxStatus.Text = "NOT DETECTED";
+			// 
+			// label_CursorForceStatusLabel
+			// 
+			this.label_CursorForceStatusLabel.AutoSize = true;
+			this.label_CursorForceStatusLabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+			this.label_CursorForceStatusLabel.Font = new System.Drawing.Font("Segoe UI", 12F);
+			this.label_CursorForceStatusLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(235)))), ((int)(((byte)(235)))));
+			this.label_CursorForceStatusLabel.Location = new System.Drawing.Point(12, 71);
+			this.label_CursorForceStatusLabel.Name = "label_CursorForceStatusLabel";
+			this.label_CursorForceStatusLabel.Size = new System.Drawing.Size(102, 21);
+			this.label_CursorForceStatusLabel.TabIndex = 0;
+			this.label_CursorForceStatusLabel.Text = "Cursor Force:";
+			// 
+			// label_CursorForceStatus
+			// 
+			this.label_CursorForceStatus.AutoSize = true;
+			this.label_CursorForceStatus.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+			this.label_CursorForceStatus.Font = new System.Drawing.Font("Segoe UI", 12F);
+			this.label_CursorForceStatus.ForeColor = System.Drawing.Color.Red;
+			this.label_CursorForceStatus.Location = new System.Drawing.Point(120, 71);
+			this.label_CursorForceStatus.Name = "label_CursorForceStatus";
+			this.label_CursorForceStatus.Size = new System.Drawing.Size(80, 21);
+			this.label_CursorForceStatus.TabIndex = 1;
+			this.label_CursorForceStatus.Text = "DISABLED";
+			// 
+			// novaPictureBox1
+			// 
+			this.novaPictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.novaPictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(75)))), ((int)(((byte)(75)))), ((int)(((byte)(75)))));
+			this.novaPictureBox1.Location = new System.Drawing.Point(12, 107);
+			this.novaPictureBox1.Name = "novaPictureBox1";
+			this.novaPictureBox1.Size = new System.Drawing.Size(225, 1);
+			this.novaPictureBox1.TabIndex = 2;
+			this.novaPictureBox1.TabStop = false;
+			// 
+			// novaLabel1
+			// 
+			this.novaLabel1.AutoSize = true;
+			this.novaLabel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+			this.novaLabel1.Font = new System.Drawing.Font("Segoe UI", 12F);
+			this.novaLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(235)))), ((int)(((byte)(235)))));
+			this.novaLabel1.Location = new System.Drawing.Point(13, 123);
+			this.novaLabel1.Name = "novaLabel1";
+			this.novaLabel1.Size = new System.Drawing.Size(151, 21);
+			this.novaLabel1.TabIndex = 3;
+			this.novaLabel1.Text = "Toggle Cursor Force:";
+			// 
+			// novaLabel2
+			// 
+			this.novaLabel2.AutoSize = true;
+			this.novaLabel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+			this.novaLabel2.Font = new System.Drawing.Font("Segoe UI", 12F);
+			this.novaLabel2.ForeColor = System.Drawing.Color.CornflowerBlue;
+			this.novaLabel2.Location = new System.Drawing.Point(170, 123);
+			this.novaLabel2.Name = "novaLabel2";
+			this.novaLabel2.Size = new System.Drawing.Size(65, 21);
+			this.novaLabel2.TabIndex = 4;
+			this.novaLabel2.Text = "CTRL+F";
+			// 
+			// Main
+			// 
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+			this.CanResize = false;
+			this.ClientSize = new System.Drawing.Size(249, 156);
+			this.Controls.Add(this.novaLabel2);
+			this.Controls.Add(this.novaLabel1);
+			this.Controls.Add(this.novaPictureBox1);
+			this.Controls.Add(this.label_CursorForceStatus);
+			this.Controls.Add(this.label_RobloxStatus);
+			this.Controls.Add(this.label_CursorForceStatusLabel);
+			this.Controls.Add(this.label_RobloxStatusLabel);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+			this.MaximizeBox = false;
+			this.MinimumSize = new System.Drawing.Size(231, 109);
+			this.Name = "Main";
+			this.ShowIcon = false;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Roblox Cursor Force";
+			((System.ComponentModel.ISupportInitialize)(this.novaPictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private NovaUI.Controls.NovaLabel label_RobloxStatusLabel;
+		private NovaUI.Controls.NovaLabel label_RobloxStatus;
+		private NovaUI.Controls.NovaLabel label_CursorForceStatusLabel;
+		private NovaUI.Controls.NovaLabel label_CursorForceStatus;
+		private NovaUI.Controls.NovaPictureBox novaPictureBox1;
+		private NovaUI.Controls.NovaLabel novaLabel1;
+		private NovaUI.Controls.NovaLabel novaLabel2;
+	}
+}
+
---
+++ Main.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+using NovaUI.Controls;
+
+namespace RBXCursorForce
+{
+	public partial class Main : NovaWindow
+	{
+		#region Win32
+
+		[StructLayout(LayoutKind.Sequential)]
+		private struct RECT
+		{
+			public int Left;
+			public int Top;
+			public int Right;
+			public int Bottom;
+		}
+
+		[StructLayout(LayoutKind.Sequential)]
+		private struct POINT
+		{
+			public int X;
+			public int Y;
+
+			public static implicit operator Point(POINT p) => new Point(p.X, p.Y);
+		}
+
+		private enum FS_MOD : uint
+		{
+			NONE = 0x0,
+			ALT = 0x1,
+			CTRL = 0x2,
+			SHIFT = 0x4
+		}
+
+		[DllImport("user32.dll")]
+		private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+		[DllImport("user32.dll")]
+		private static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+
+		[DllImport("user32.dll")]
+		private static extern bool RegisterHotKey(IntPtr hWnd, int id, FS_MOD fsModifiers, Keys vk);
+
+		[DllImport("user32.dll")]
+		private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+		[DllImport("user32.dll")]
+		[return: MarshalAs(UnmanagedType.Bool)]
+		private static extern bool SetCursorPos(int x, int y);
+
+		#endregion
+
+		private IntPtr _rbxptr = IntPtr.Zero;
+		private bool _forceCursor = false;
+
+		public Main()
+		{
+			InitializeComponent();
+
+			Timer t = new Timer { Interval = 1 };
+			t.Tick += (_, __) =>
+			{
+				_rbxptr = FindWindow(null, "Roblox");
+
+				if (_rbxptr != IntPtr.Zero || _rbxptr != (IntPtr)0)
+				{
+					GetWindowRect(_rbxptr, out RECT rect);
+
+					int x = rect.Left;
+					int y = rect.Top;
+					int w = (rect.Right - rect.Left) / 2;
+					int h = (rect.Bottom - rect.Top) / 2;
+
+					if (_forceCursor) SetCursorPos(x + w, y + h);
+				}
+
+				GC.Collect();
+
+				label_RobloxStatus.Text = $"{(_rbxptr != IntPtr.Zero || _rbxptr != (IntPtr)0 ? "" : "NOT ")}DETECTED";
+				label_RobloxStatus.ForeColor = _rbxptr != IntPtr.Zero || _rbxptr != (IntPtr)0 ? Color.SeaGreen : Color.Red;
+
+				label_CursorForceStatus.Text = _forceCursor ? "ENABLED" : "DISABLED";
+				label_CursorForceStatus.ForeColor = _forceCursor ? Color.SeaGreen : Color.Red;
+			};
+			t.Start();
+		}
+
+		protected override void OnLoad(EventArgs e)
+		{
+			base.OnLoad(e);
+
+			int id_toggle = 0xF0AC;
+
+			bool hk_toggle = RegisterHotKey(Handle, id_toggle, FS_MOD.CTRL, Keys.F);
+
+			if (!hk_toggle)
+				MessageBox.Show("Failed to register 'toggle' hotkey: CTRL + SHIFT + F", "HotKey Not Binded",
+					MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+		}
+
+		protected override void OnFormClosing(FormClosingEventArgs e)
+		{
+			base.OnFormClosing(e);
+
+			UnregisterHotKey(Handle, 0xF0AC);
+		}
+
+		protected override void WndProc(ref Message m)
+		{
+			base.WndProc(ref m);
+
+			const uint WM_HOTKEY = 0x312;
+
+			if (m.Msg == WM_HOTKEY && (int)m.WParam == 0xF0AC)
+				_forceCursor = !_forceCursor;
+		}
+	}
+}
